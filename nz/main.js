881af5d82b3eeadab35fd86718a6aa0d108feddf
David Reisenauer <David@Davids-MacBook-Pro-3.local>
2020-03-30 12:13:25 +0200

    Map Setup

---
+++ nz/main.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([-40.833333, 172.9], 13);
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+}).addTo(map);
