44df9156e792fd6ce840179d290f5b2ee478f166
David Reisenauer <David@Davids-MacBook-Pro-3.local>
2020-03-30 19:51:17 +0200

    Code zur Erstellung der Karte ausgebessert

--- nz/main.js
+++ nz/main.js
@@ -1,5 +1,9 @@
-var map = L.map('map').setView([-40.833333, 172.9], 13);
+let map = document.querySelector("#map")
+var mymap = L.map('map').setView([-40.928064, 172.990424], 13);
+
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     maxZoom: 17,
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-}).addTo(map);
+}).addTo(mymap);
+
+var marker = L.marker([-40.928064, 172.990424]).addTo(mymap)
